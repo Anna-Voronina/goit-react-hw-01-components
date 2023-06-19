c42b557e8ec18e5e7c96f050d683556b5177ea3f
Anna-Voronina <anna.vs2512@gmail.com>
2023-06-19 21:26:42 +0300

    Update profile component

---
+++ src/components/ProfileStats/ProfileStats.jsx
@@ -0,0 +1,16 @@
+import css from './ProfileStats.module.css';
+
+export const ProfileStats = ({ stats }) => {
+  return (
+    <ul className={css.stats}>
+      {Object.entries(stats).map(([key, value]) => {
+        return (
+          <li key={key}>
+            <span className={css.label}>{key}</span>
+            <span className={css.quantity}>{value}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
