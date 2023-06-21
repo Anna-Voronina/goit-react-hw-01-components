5634c34baf81079007525a81881b1969a25d9606
Anna-Voronina <anna.vs2512@gmail.com>
2023-06-21 09:17:49 +0300

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,11 +4,9 @@ export const FriendsItem = ({ friendInfo: { name, isOnline, avatar } }) => {
   return (
     <li className={css.friendsItem}>
       <span
-        className={
-          isOnline
-            ? `${css.status} ${css.online}`
-            : `${css.status} ${css.offline}`
-        }
+        className={`${css.status} ${
+          isOnline ? `${css.online}` : `${css.offline}`
+        }`}
       ></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
